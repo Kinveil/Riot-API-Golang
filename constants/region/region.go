07859e445dad9e3fbdba959440478dccc708812c
Junior Yono <jryono123@gmail.com>
2023-12-12 16:36:58 -0800

    Fixed region from string

--- constants/region/region.go
+++ constants/region/region.go
@@ -64,22 +64,22 @@ func (r Region) String() string {
 }
 
 var stringToRegion = map[string]Region{
-	"BR1":  BR1,
-	"EUN1": EUN1,
-	"EUW1": EUW1,
-	"JP1":  JP1,
-	"KR":   KR,
-	"LA1":  LA1,
-	"LA2":  LA2,
-	"NA1":  NA1,
-	"OC1":  OC1,
-	"PH2":  PH2,
-	"RU":   RU,
-	"SG2":  SG2,
-	"TH2":  TH2,
-	"TR1":  TR1,
-	"TW2":  TW2,
-	"VN2":  VN2,
+	"BR":  BR1,
+	"EUN": EUN1,
+	"EUW": EUW1,
+	"JP":  JP1,
+	"KR":  KR,
+	"LAN": LA1,
+	"LAS": LA2,
+	"NA":  NA1,
+	"OC":  OC1,
+	"PH":  PH2,
+	"RU":  RU,
+	"SG":  SG2,
+	"TH":  TH2,
+	"TR":  TR1,
+	"TW":  TW2,
+	"VN":  VN2,
 }
 
 func FromString(rgn string) Region {
