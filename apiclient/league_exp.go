9b2d0c8aa1709fbdd106bac6686340c6b6f8f8fc
Junior Yono <jryono123@gmail.com>
2024-02-17 00:53:06 -0800

    Update league API and league exp API functions

--- apiclient/league_exp.go
+++ apiclient/league_exp.go
@@ -2,15 +2,16 @@ package apiclient
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/Kinveil-Engineering-Analysis/Riot-API-Golang/apiclient/ratelimiter"
+	"github.com/Kinveil-Engineering-Analysis/Riot-API-Golang/constants/league/rank"
+	"github.com/Kinveil-Engineering-Analysis/Riot-API-Golang/constants/league/tier"
 	"github.com/Kinveil-Engineering-Analysis/Riot-API-Golang/constants/queue_ranked"
 	"github.com/Kinveil-Engineering-Analysis/Riot-API-Golang/constants/region"
 )
 
-func (c *client) GetLeagueExpEntries(r region.Region, q queue_ranked.String, tier, division string, page int) ([]LeagueEntry, error) {
+func (c *client) GetLeagueExpEntries(r region.Region, q queue_ranked.String, tier tier.String, rank rank.String, page int) ([]LeagueEntry, error) {
 	var res []LeagueEntry
-	_, err := c.dispatchAndUnmarshal(r, "/lol/league-exp/v4/entries", fmt.Sprintf("/%s/%s/%s?page=%d", string(q), strings.ToUpper(tier), strings.ToUpper(division), page), nil, ratelimiter.GetLeagueExpEntries, &res)
+	_, err := c.dispatchAndUnmarshal(r, "/lol/league-exp/v4/entries", fmt.Sprintf("/%s/%s/%s?page=%d", q, tier, rank, page), nil, ratelimiter.GetLeagueExpEntries, &res)
 	return res, err
 }
